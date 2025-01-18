1ed2fbb56d3d1da265d02432cdf86a27a59b6044
Shreshth Sharma <102848493+ShrenzyPanda@users.noreply.github.com>
2025-01-18 23:19:00 +0530

    CLI Tool for Baseline has been added with AI Agents

--- ML/cli_baseline.py
+++ ML/cli_baseline.py
@@ -26,7 +26,7 @@ class Agent():
 utilitarian_system = "You are an AI agent that prioritizes utilitarian ethics and tells the course of action for a given situtation."
 deontological_system = "You are an AI agent that prioritizes deontological ethics and tells the course of action for a given situtation."
 virtue_system = "You are an AI agent that prioritizes virtue ethics and tells the course of action for a given situtation."
-default_system = "You are an AI agent that tells the course of action for a given situation which generally mimics a human behaviour and their inherent biases."
+default_system = "You are an AI agent that is similar to a human who prioritzes themselves over others and the societal good and tells the course of action for a given situation."
 
 utilitarian_agent = Agent(model=model_path, system_prompt=utilitarian_system)
 deontological_agent = Agent(model=model_path, system_prompt=deontological_system)
@@ -35,23 +35,30 @@ default_agent = Agent(model=model_path, system_prompt=default_system)
 
 def main():
     prompt = input("Enter the prompt describing the moral dilemma:\n")
-    framework = input("Enter the psychological framework: 'u' for Utilitarianism, 'd' for Deontological, 'v' for Virtue, and press enter for skipping this step\n")
+    framework = input("Enter the psychological framework: 'u' -> Utilitarianism, 'd' -> Deontology, 'v' -> Virtue, 'a' -> All of the above and press enter for skipping this step\n")
 
+    response_dict = {}
     if framework == 'u':
-        response_dict = utilitarian_agent.invoke(prompt=prompt)
         response_dict["prompt"] = prompt
+        response_dict['response'] = utilitarian_agent.invoke(prompt=prompt)["response"]
     elif framework == 'd':
-        response_dict = deontological_agent.invoke(prompt=prompt)
         response_dict["prompt"] = prompt
+        response_dict["response"] = deontological_agent.invoke(prompt=prompt)["response"]
     elif framework == 'v':
-        response_dict = virtue_agent.invoke(prompt=prompt)
         response_dict["prompt"] = prompt
+        response_dict["response"] = virtue_agent.invoke(prompt=prompt)["response"]
+    elif framework == 'a':
+        response_dict["prompt"] = prompt
+        response_dict["response"] = "\nUtilitarianism: " + utilitarian_agent.invoke(prompt=prompt)["response"] + \
+        "\nDeontology: " + deontological_agent.invoke(prompt=prompt)["response"] + \
+        "\nVirtue: " + virtue_agent.invoke(prompt=prompt)["response"] + \
+        "\nDefault: " + default_agent.invoke(prompt=prompt)["response"]
     else:
         response_dict = default_agent.invoke(prompt=prompt)
         response_dict["prompt"] = prompt
 
     print(f"{response_dict['response']}\n")
-    
+
     json_file = "response.json"
     with open(json_file, "w") as outfile: 
         json.dump(response_dict, outfile)
